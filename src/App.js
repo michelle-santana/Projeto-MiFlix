54949ee8e4034bc41b311f5b97f364eb1b916b29
michelle-santana <michelle.raiadrogasil@gmail.com>
2020-08-03 17:31:50 -0300

    progredindo...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import Menu from './Componentes/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from '../src/Componentes/BannerMain';
+import Carousel from './Componentes/Carousel';
+import Footer from './Componentes/Footer';
 
 
 
+const Home = () => {
+    return (
+      <>
+      <Menu />
+  
+      <BannerMain
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={"Um dos melhores animes da vida!"}
+      />
+  
+      <Carousel
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[0]}
+      />
+  
+      <Carousel
+          category={dadosIniciais.categorias[1]}
+      />
+  
+      <Carousel
+          category={dadosIniciais.categorias[2]}
+      />      
+  
+      <Carousel
+          category={dadosIniciais.categorias[3]}
+      />           
+  
+      <Carousel
+          category={dadosIniciais.categorias[4]}
+      />           
+  
+      <Carousel
+          category={dadosIniciais.categorias[5]}
+      />           
+  
+      <Footer />
+  </>
 
-function App() {
-  return (
-    <div>
-      <Menu/>
-    </div>
-  );
-}
+    );
 
-export default App;
+  }
+
+
+export default Home;
--- src/Componentes/Footer/index.js
+++ src/Componentes/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Site criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
